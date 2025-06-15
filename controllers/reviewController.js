3c4431f2ecdcd0bc8c37d8f9f0b1f08a3099edfe
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2025-06-15 15:50:39 +0300

    feat: add population support for user details in review operations

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,14 +2,17 @@ const asyncHandler = require("express-async-handler");
 const ReviewModel = require("../models/reviewModel");
 const factory = require("../utils/factory");
 
+const population = { path: "user", select: "name" };
+
 exports.createReview = factory.createDocument(ReviewModel, {
   preTask: asyncHandler(async (req) => (req.body.user = req.user._id)), // User ID is required.
+  populate: population,
 });
 
-exports.getReviews = factory.getAllDocuments(ReviewModel);
+exports.getReviews = factory.getAllDocuments(ReviewModel, { populate: population });
 
-exports.getReview = factory.getDocument(ReviewModel);
+exports.getReview = factory.getDocument(ReviewModel, { populate: population });
 
-exports.updateReview = factory.updateDocument(ReviewModel);
+exports.updateReview = factory.updateDocument(ReviewModel, { populate: population });
 
 exports.deleteReview = factory.deleteDocument(ReviewModel);
